f9837241edcff4696ce8699c9b35767341453cb9
Oliver Calazans <oliver.calazans@gmail.com>
2026-02-01 19:46:21 -0300

    fix bugs and refac for better threads usage

--- ds-spider.py
+++ ds-spider.py
@@ -1,7 +1,8 @@
 import os
 import requests
 import sys
-from threading    import Lock
+import time
+from threading    import Lock, Thread
 from queue        import Queue, Empty
 from io           import BytesIO
 from ds_store     import DSStore
@@ -9,112 +10,105 @@ from urllib.parse import urlparse
 
 
 
-
-def main() -> None:    
-    if len(sys.argv) <= 1:
-        print('[ ERROR ] Missing argument/URL')
-        sys.exit(1)
-
-    sys.argv[1]        
-    for _ in range(10):
-        s = DSSpider()
-        s.scan()
-
-
-
 class DSSpider:
 
-    DIRECTORY: str  = os.path.abspath('.')
-
-
     def __init__(self):
-        url = self._get_url()
+        url = self._get_args()
 
         self._queue: Queue = Queue()
         self._queue.put(url)
         
-        self._processed_urls:  set  = set()
-        self._lock:            Lock = Lock()
-        self._working_threads: int  = 0
+        self._lock:           Lock = Lock()
+        self._threads:        int  = 0
+        self._DIR:            str  = os.path.abspath('.')
+        self._processed_urls: set  = set()
 
-        self._running: bool = True
 
 
     
     @staticmethod
-    def _get_url() -> str:
+    def _get_args() -> str:
         if len(sys.argv) <= 1:
+            print('[ ERROR ] Missing argument/URL')
+            print('[ USAGE ] python ds-spider.py http://example.com/.DS_Store')
             sys.exit(1)
 
         return sys.argv[1]
+    
+
+
+    def run(self):
+        print('[+] Crawler started\n')
+        self._add_thread()
+        
+        while self._threads > 0:
+            time.sleep(0.5)
+        
+        print('\n[-] Crawler finished')
 
 
     
-    def _display(self, msg):
+    def _add_thread(self):
+        if self._threads >= 10:
+            return
+
         with self._lock:
-            print(msg)
+            self._threads += 1
+            t = Thread(target=self._scan)
+            t.daemon = False
+            t.start()
+
+
+    
+    def _remove_thread(self):
+        with self._lock: 
+            self._threads -= 1
 
 
 
-    def _process(self) -> None:
-        while self._running:
-            try:
-                url = self._get_url()
-            except Empty:
-                self._check_threads()
-                continue
-            except Exception:
-                self._display(f'[ERROR] _get_url: {e}')
-                continue                
+    def _scan(self):        
+        while True:
+            url = self._get_url()
+
+            if not url:
+                self._remove_thread()
+                break
 
             if url in self._processed_urls:
                 continue
             
-            url, base_url            = self._parse_url(url)
-            _, netloc, path, _, _, _ = urlparse(url, 'http')    
-            response                 = self._download(url)
+            url, base_url = self._parse_url(url)
+            netloc, path  = self._split_url(url)    
+            response      = self._download(url)
 
             if response is None or response.status_code != 200:
-                self._working_threads -= 1
                 continue
 
-            try:
-                self._create_folder(netloc, path)
-                self._save_file(netloc, path, response, url)
-                
-                if not url.endswith('.DS_Store'):
-                    continue
-                
-                self._process_ds_store_file(response, base_url)
+            self.processes_response(netloc, path, response, url, base_url)
 
-            except Exception as e:
-                self._display('[ERROR] %s' % str(e))
-            finally:
-                self._working_threads -= 1
 
-    
 
-    def _get_url(self) -> str:
+    def _get_url(self) -> str | None:
         try:
             url = self._queue.get(timeout=2.0)
-            self._lock.acquire()
-            self._working_threads += 1
-            self._lock.release()
             return url
         except Empty:
-            raise
+            return None
         except Exception as e:
-            self._display(f'[ERROR] queue.get: {e}')
-            raise
+            self._display(f'[ ERROR ] queue.get: {e}')
+            return None
     
 
 
-    def _check_threads(self) -> None:
-        if self._working_threads != 0:
-            return
-        
+    def _split_url(self, url) -> tuple[str]:
+        _, netloc, path, _, _, _ = urlparse(url, 'http')
+        return netloc, path
+
+    
+
+    def _display(self, msg):
         with self._lock:
-            self._running = False
+            print(msg)
             
 
 
@@ -131,24 +125,42 @@ class DSSpider:
 
     def _download(self, url):
         try:
-            return requests.get(url, allow_redirects=False)
+            return requests.get(url, allow_redirects=False, timeout=10)
         except Exception as e:            
-            self._display('[ERROR] %s' % str(e))            
+            self._display('[ ERROR ] %s' % str(e))            
             return None
     
+
+
+    def processes_response(self, netloc, path, response, url, base_url):
+        try:
+            self._create_folder(netloc, path)
+            self._save_file(netloc, path, response, url)
+                
+            if not url.endswith('.DS_Store'):
+                return
+                
+            self._process_ds_store_file(response, base_url)
+
+        except Exception as e:
+            self._display('[ ERROR ] %s' % str(e))
+
 
 
     def _create_folder(self, netloc, path):
         folder_name = netloc.replace(':', '_') + '/'.join(path.split('/')[:-1])
         
         if not os.path.exists(folder_name):
-            os.makedirs(folder_name)
+            os.makedirs(folder_name, exist_ok=True)
 
     
 
     def _save_file(self, netloc, path, response, url):
-        with open(netloc.replace(':', '_') + path, 'wb') as out_file:
-            self._display('[%s] %s' % (response.status_code, url))
+        filename = netloc.replace(':', '_') + path
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        
+        with open(filename, 'wb') as out_file:
+            self._display(f'[{response.status_code}] {url}')
             out_file.write(response.content)
     
 
@@ -169,11 +181,11 @@ class DSSpider:
                 continue
                 
             self._queue.put(base_url + name)
+            self._add_thread()
     
-            if len(name) > 4 and name[-4] == '.':
-                continue
-
-            self._queue.put(base_url + name + '/.DS_Store')
+            if '.' not in name or len(name.split('.')[-1]) > 4:
+                self._queue.put(base_url + name + '/.DS_Store')
+                self._add_thread()
                 
         d.close()
 
@@ -182,11 +194,11 @@ class DSSpider:
     def _is_valid_name(self, entry_name):
         validation = entry_name.find('..') >= 0 or entry_name.startswith('/')
         validation = validation or entry_name.startswith('\\')
-        validation = validation or not os.path.abspath(entry_name).startswith(self.DIRECTORY)
+        validation = validation or not os.path.abspath(entry_name).startswith(self._DIR)
         
         if validation:
             try:
-                print('[ERROR] Invalid entry name: %s' % entry_name)
+                print('[ ERROR ] Invalid entry name: %s' % entry_name)
             except Exception:
                 pass
 
@@ -199,4 +211,5 @@ class DSSpider:
 
 
 if __name__ == '__main__':
-    main()
+    spider = DSSpider()
+    spider.run()
